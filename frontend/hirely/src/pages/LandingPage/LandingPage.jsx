0bb0d0295ac37cbc17038bf33564c03cf98fdf24
KikoNassif <ktamer2003@gmail.com>
2025-08-01 11:59:58 +0400

    Implemented Header section in landing page

--- frontend/hirely/src/pages/LandingPage/LandingPage.jsx
+++ frontend/hirely/src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='min-h-screen'>
+      <Header />
+    </div>
   )
 }
 
