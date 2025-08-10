3c61d8bdf1a9de3292393d907d720751483faa0a
KikoNassif <ktamer2003@gmail.com>
2025-08-10 21:12:40 +0400

    Implemented authentication API's

---
+++ backend/models/Application.js
@@ -0,0 +1,17 @@
+const mongoose = required("mongoose");
+
+const applicationSchema = new mongoose.Schema(
+  {
+    job: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
+    applicant: { type: mongoose.Schema.types.ObjectId, ref: "User", required: true },
+    resume: { type: String }, // can store uploaded version or link
+    status: {
+      type: String,
+      enum: ["Applied", "In Review", "Rejected", "Accepted"],
+      default: "Applied"
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Application", applicationSchema);
--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ connectDB();
 app.use(express.json());
 
 // Routes
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 //Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
