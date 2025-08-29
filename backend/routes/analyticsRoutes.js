9a5b64f0e692277ac7d970f32d1480776d5848da
KikoNassif <ktamer2003@gmail.com>
2025-08-29 09:44:28 +0400

    Implemented employer dashboard

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
-const { getEmployerAnalytics } = require("../controllers/analticsController");
+const { getEmployerAnalytics } = require("../controllers/analyticsController");
 
 router.get("/overview", protect, getEmployerAnalytics);
 
