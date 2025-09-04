d965141cc39636376368c57506795c0a3b0077ca
KikoNassif <ktamer2003@gmail.com>
2025-09-04 14:57:14 +0400

    Implemented View Applications page

--- frontend/hirely/src/utils/helper.js
+++ frontend/hirely/src/utils/helper.js
@@ -33,3 +33,12 @@
 
     return "";
   };
+
+  export const getInitials = (name) => {
+    return name
+      .split(" ")
+      .map((word) => word.charAt(0))
+      .join("")
+      .toUpperCase()
+      .slice(0, 2);
+  };
